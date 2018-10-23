b0045288bfdd6e7f9f630bee0a4cb4ee9fe03508
Thomas Murphy <tmurph21@gmail.com>
2018-10-23 16:49:40 -0500

    add publish methods for creating, updating, and deleting posts for subscription

--- basics/src/resolvers/Mutation.js
+++ basics/src/resolvers/Mutation.js
@@ -71,12 +71,20 @@ const Mutation = {
 
     db.posts.push(post);
 
-    if (post.published) pubsub.publish('post', { post });
+    if (post.published) {
+      pubsub.publish('post', {
+        post: {
+          mutation: 'CREATED',
+          data: post
+        }
+      });
+    }
 
     return post;
   },
-  updatePost(parent, args, { db }, info) {
+  updatePost(parent, args, { db, pubsub }, info) {
     const post = db.posts.find(pst => pst.id === args.id);
+    const originalPost = { ...post };
 
     if (!post) throw new Error('Post not found');
 
@@ -90,20 +98,52 @@ const Mutation = {
 
     if (typeof args.data.published === 'boolean') {
       post.published = args.data.published;
+
+      if (originalPost.published && !post.published) {
+        pubsub.publish('post', {
+          post: {
+            mutation: 'DELETED',
+            data: originalPost
+          }
+        });
+      } else if (!originalPost.published && post.published) {
+        pubsub.publish('post', {
+          post: {
+            mutation: 'CREATED',
+            data: post
+          }
+        });
+      }
+    } else if (post.published) {
+      pubsub.publish('post', {
+        post: {
+          mutation: 'UPDATED',
+          data: post
+        }
+      });
     }
 
     return post;
   },
-  deletePost(parent, args, { db }, info) {
+  deletePost(parent, args, { db, pubsub }, info) {
     const postIndex = db.posts.findIndex(post => post.id === args.id);
 
     if (postIndex === -1) throw new Error('Post not found');
 
-    const deletedPosts = db.posts.splice(postIndex, 1);
+    const [post] = db.posts.splice(postIndex, 1);
 
     db.comments = db.comments.filter(comment => comment.post !== args.id);
 
-    return deletedPosts[0];
+    if (post.published) {
+      pubsub.publish('post', {
+        post: {
+          mutation: 'DELETED',
+          data: post
+        }
+      });
+    }
+
+    return post;
   },
   createComment(parent, args, { db, pubsub }, info) {
     const userExists = db.users.some(user => user.id === args.data.author);
