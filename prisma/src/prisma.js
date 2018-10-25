c511e0bd4c40f8f1eff6b67e7f5ae4c990e76cfc
Thomas Murphy <tmurph21@gmail.com>
2018-10-25 11:09:30 -0500

    add prisma-binding methods and add @relation to datamodel

--- prisma/src/prisma.js
+++ prisma/src/prisma.js
@@ -5,10 +5,65 @@ const prisma = new Prisma({
   endpoint: 'http://localhost:4466'
 });
 
-prisma.query.users(null, '{ id name email posts { id title } }').then((data) => {
-  console.log(JSON.stringify(data, null, 2));
-}).catch(e => console.log(e));
+const createPostForUser = async (authorId, data) => {
+  const userExists = await prisma.exists.User({
+    id: authorId
+  });
 
-prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
-  console.log(JSON.stringify(data, null, 2));
-}).catch(e => console.log(e));
+  if (!userExists) throw new Error('User not found');
+
+  try {
+    const post = await prisma.mutation.createPost({
+      data: {
+        ...data,
+        author: {
+          connect: {
+            id: authorId
+          }
+        }
+      }
+    }, '{ id title body published author { id name email posts { id title } } }');
+
+    return post;
+  } catch (e) {
+    return e;
+  }
+};
+
+const updatePostForUser = async (postId, data) => {
+  const postExists = await prisma.exists.Post({
+    id: postId
+  });
+
+  if (!postExists) throw new Error('Post not found');
+
+  try {
+    const post = await prisma.mutation.updatePost({
+      where: {
+        id: postId
+      },
+      data: {
+        ...data
+      }
+    }, '{ id title body published author { id name email posts { id title } } }');
+
+    return post;
+  } catch (e) {
+    return e;
+  }
+};
+
+// createPostForUser('cjnnn5qnt00540863dz6vhhe0', {
+//   title: 'Post',
+//   body: 'Body',
+//   published: true
+// }).then((user) => {
+//   console.log(JSON.stringify(user, null, 2));
+// }).catch(e => console.log(e.message));
+
+// updatePostForUser('cjnoqhkw9007l08631ot8ujwp', {
+//   title: 'Async Await Title',
+//   body: 'Async Await Body'
+// }).then((post) => {
+//   console.log(post);
+// }).catch(e => console.log(e.message));
